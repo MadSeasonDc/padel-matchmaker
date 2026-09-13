cb811a48a5cc4e7c46778846123e3b0006b4d7d8
MadSeasonDc <manueldiazcanotr@gmail.com>
2026-09-13 18:09:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -709,7 +709,7 @@ def generar_pdf_results(jornada):
     # SUBTÍTULO
     # =========================
     c.setFont("Helvetica", 11)
-    c.drawCentredString(width / 2, y, "Liga de Pádel Atos MEV")
+    c.drawCentredString(width / 2, y, "Icebox Padel League")
     y -= 22
 
     # =========================
@@ -746,7 +746,7 @@ def generar_pdf_results(jornada):
             y -= 31
 
             c.setFont("Helvetica", 11)
-            c.drawCentredString(width / 2, y, "Liga de Pádel Atos MEV")
+            c.drawCentredString(width / 2, y, "Icebox Padel League")
             y -= 22
 
             c.line(left, y, right, y)
@@ -919,7 +919,7 @@ def generar_pdf_schedule(jornada):
     # SUBTÍTULO
     # =========================
     c.setFont("Helvetica", 11)
-    c.drawCentredString(width / 2, y, "Liga de Pádel Atos MEV")
+    c.drawCentredString(width / 2, y, "Icebox Padel League")
     y -= 22
 
     # =========================
@@ -955,7 +955,7 @@ def generar_pdf_schedule(jornada):
             y -= 31
 
             c.setFont("Helvetica", 11)
-            c.drawCentredString(width / 2, y, "Liga de Pádel Empresa")
+            c.drawCentredString(width / 2, y, "Icebox Padel League")
             y -= 22
 
             c.line(left, y, right, y)
@@ -1091,7 +1091,7 @@ def generar_pdf_ranking(ranking_rows):
 
     # ---------- SUBTÍTULO ----------
     c.setFont("Helvetica", 11)
-    c.drawCentredString(width / 2, y, "Liga de Pádel Atos MEV")
+    c.drawCentredString(width / 2, y, "Icebox Padel League")
     y -= 22
 
     # ---------- LÍNEA ----------
